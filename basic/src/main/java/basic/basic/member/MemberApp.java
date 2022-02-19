7a16e08b645bc06c19d4d2b772417a0482ccd02b
dkswnkk <jjoy9810@gmail.com>
2022-02-19 22:34:10 +0900

    test: 회원 도메인 실행과 테스트

---
+++ basic/src/main/java/basic/basic/member/MemberApp.java
@@ -0,0 +1,12 @@
+package basic.basic.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+    }
+}
---
+++ basic/src/test/java/basic/basic/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package basic.basic.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "안주형", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
+}
