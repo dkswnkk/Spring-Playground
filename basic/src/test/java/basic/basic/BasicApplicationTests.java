f81b00dab13a87b53915bfe65ce19ade09237e2f
dkswnkk <jjoy9810@gmail.com>
2022-02-19 20:35:51 +0900

    chore: Spring Basic 프로젝트 생성

---
+++ basic/src/test/java/basic/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
