5df6ed608696292c12c5118bc5321851a5c6f35e
dkswnkk <jjoy9810@gmail.com>
2022-02-19 10:26:18 +0900

    feat: 회원 서비스 개발

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemverService.java
@@ -0,0 +1,42 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemverService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        // Null값이 아니면 예외처리(이미 존재한다는 뜻)
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
