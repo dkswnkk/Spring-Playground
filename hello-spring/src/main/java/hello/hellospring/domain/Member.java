195adea672bb15c74b823cba2729a1867dc97a5c
dkswnkk <jjoy9810@gmail.com>
2022-02-19 09:21:17 +0900

    chore: 회원 도메인과 리포지토리 만들기

---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,20 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
