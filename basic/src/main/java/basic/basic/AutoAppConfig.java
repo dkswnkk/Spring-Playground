a5aebb53e2e255039239f7478c33e4e38b994a89
dkswnkk <jjoy9810@gmail.com>
2022-02-23 17:22:20 +0900

    chore: 다양한 의존관계 주입 방법

--- basic/src/main/java/basic/basic/AutoAppConfig.java
+++ basic/src/main/java/basic/basic/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
 //        basePackages = "basic.basic.member",
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
--- basic/src/main/java/basic/basic/order/OrderServiceImpl.java
+++ basic/src/main/java/basic/basic/order/OrderServiceImpl.java
@@ -9,14 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
-    private final MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
