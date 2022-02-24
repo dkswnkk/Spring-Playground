71a80b394f856c7bc444aade3cf5572b78f5adce
dkswnkk <jjoy9810@gmail.com>
2022-02-24 18:23:44 +0900

    refactor: 애노테이션 @PostConstruct, @PreDestroy

--- basic/src/test/java/basic/basic/lifecycle/BeanLifeCycleTest.java
+++ basic/src/test/java/basic/basic/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://www.naver.com");
--- basic/src/test/java/basic/basic/lifecycle/NetworkClient.java
+++ basic/src/test/java/basic/basic/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package basic.basic.lifecycle;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -26,12 +29,13 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
-
+    @PostConstruct
     public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         disconnect();
     }
