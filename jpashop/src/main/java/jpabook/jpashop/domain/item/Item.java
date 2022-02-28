bebdcf83a6b2fc763a4c9e58b41bc5ad37bad301
dkswnkk <jjoy9810@gmail.com>
2022-02-28 12:35:55 +0900

    feat: 엔티티 구현

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn    //부모 클래스에 선언한다.
+@Getter
+@Setter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "item")
+    private List<Category> categories = new ArrayList<>();
+
+
+}
