bcc2de4d2392a856a8895fe43c4e16b275fe5f74
dkswnkk <jjoy9810@gmail.com>
2022-02-28 13:42:37 +0900

    feat: 연관관계 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -27,10 +27,17 @@ public class Category {
     )
     private List<Item> item = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
+    
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +34,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태: [ORDER, CANCEL]
+
+    //==연관괸계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
 }
