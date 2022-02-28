f68450897f92cf23ba158f0f6761523ddb1b2072
dkswnkk <jjoy9810@gmail.com>
2022-02-28 20:38:24 +0900

    feat: 상품 비즈니스 로직 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,21 @@ public abstract class Item {
     @ManyToMany(mappedBy = "item")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==// -> 데이터를 가지고 있는 쪽에서 로직이 있어야 관리하기가 편하다.
+
+    /**
+     * stock(재고) 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,17 +2,12 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import java.util.Objects;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Transactional
 @SpringBootTest
