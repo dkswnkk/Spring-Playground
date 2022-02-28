de172ade469153b7ff852e56d31502f92bf2847c
dkswnkk <jjoy9810@gmail.com>
2022-02-28 20:29:12 +0900

    test: 회원 기능 테스트

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("안주형");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+
+    @Test()
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("안주형");
+
+        Member member2 = new Member();
+        member2.setName("안주형");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);    //예외가 발생한다.
+        } catch (IllegalStateException e) {
+            return;
+        }
+
+        //then
+
+    }
+}
