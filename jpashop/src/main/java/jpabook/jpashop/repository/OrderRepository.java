f38d0f7f117cb9a35912f65d2e74818302535da6
dkswnkk <jjoy9810@gmail.com>
2022-03-01 12:46:41 +0900

    feat: 주문 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,10 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.criteria.Order;
 
 @Repository
 @RequiredArgsConstructor
