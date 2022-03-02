d8241ea59bbd8896175e9ce4cd49d2d24b3ad6b6
dkswnkk <jjoy9810@gmail.com>
2022-03-02 16:43:41 +0900

    chore: jpa-basic 프로젝트 생성

---
+++ jpa-basic/src/main/java/jpabasic/jpabasic/JpaBasicApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.jpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicApplication.class, args);
+	}
+
+}
---
+++ jpa-basic/src/test/java/jpabasic/jpabasic/JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
