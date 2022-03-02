4c3a1f2d52a62ea3e7809da9814ee4bb540a656e
dkswnkk <jjoy9810@gmail.com>
2022-03-02 13:38:01 +0900

    refactor: 변경 감지와 병합(merge)

--- jpashop/src/main/java/jpabook/jpashop/controller/itemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/itemController.java
@@ -62,17 +62,16 @@ public class itemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable String itemId) {
-        Book book = new Book();
+    public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable Long itemId) {
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
 
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 
--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -26,4 +26,12 @@ public class ItemService {
         return itemRepository.findAll();
     }
 
+    @Transactional(readOnly = false)
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
 }
