3b12268c3f2b319f62ae7d06d3714f71af22784f
dkswnkk <jjoy9810@gmail.com>
2022-03-07 14:56:11 +0900

    chore: getClass 이해 및 em.persist 추가 이해 getClass는 해당 domain 클래스를 가져온다. 따라서 m1과 m2는 domain.Member 로 동일. em.persist 상태에서 id값을 생성하면서 메모리 객체인 member1 에도 값을 입력시켜준다. 따라서 1차 캐시가 지워지더라도 id값을 불러올 수 있다.

--- jpa-basic/src/main/java/jpabasic/jpabasic/domain/Delivery.java
+++ jpa-basic/src/main/java/jpabasic/jpabasic/domain/Delivery.java
@@ -2,7 +2,6 @@ package jpabasic.jpabasic.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
--- jpa-basic/src/test/java/jpabasic/jpabasic/test/NewTest.java
+++ jpa-basic/src/test/java/jpabasic/jpabasic/test/NewTest.java
@@ -1,6 +1,7 @@
 package jpabasic.jpabasic.test;
 
 import jpabasic.jpabasic.domain.Book;
+import jpabasic.jpabasic.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,9 +23,34 @@ public class NewTest {
         Book book = new Book();
         book.setName("책제목");
         book.setAuthor("안주형작가");
-
         em.persist(book);
+    }
 
+    @Test
+    public void 비교(){
+        Member member1 = new Member();
+        member1.setName("안주형");
+        System.out.println("======================");
+        System.out.println(member1.getId());
+        em.persist(member1);
+        System.out.println("====================");
+        System.out.println(member1.getId());
+
+        Member member2 = new Member();
+        member2.setName("김동까");
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        Member m1 = em.find(Member.class, member1.getId());
+        Member m2 = em.find(Member.class, member2.getId());
+
+        System.out.println(m1);
+        System.out.println(m2);
+
+        System.out.println(m1.getClass());
+        System.out.println(m2.getClass());
     }
 
 }
