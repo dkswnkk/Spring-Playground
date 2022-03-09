745733a2d25cbb076290a9802f4910884324d69f
dkswnkk <jjoy9810@gmail.com>
2022-03-09 16:21:21 +0900

    chore: JQPL 연습 프로젝트 생성

---
+++ JPQL-Basic/src/main/java/JPQLBasic/JPQLBasic/JpqlBasicApplication.java
@@ -0,0 +1,13 @@
+package JPQLBasic.JPQLBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpqlBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpqlBasicApplication.class, args);
+	}
+
+}
---
+++ JPQL-Basic/src/test/java/JPQLBasic/JPQLBasic/JpqlBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package JPQLBasic.JPQLBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
