3599320823cb641ebe15c06275cb35ac02234cf6
dkswnkk <jjoy9810@gmail.com>
2022-03-10 11:02:43 +0900

    chore: 기본 문법과 쿼리 API https://big-marjoram-ffc.notion.site/JPQL-fa265c7554484934a185d25e543ed3db

--- JPQL-Basic/src/test/java/JPQLBasic/JPQLBasic/JpqlTest.java
+++ JPQL-Basic/src/test/java/JPQLBasic/JPQLBasic/JpqlTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @SpringBootTest
 @Rollback(value = false)
@@ -17,9 +19,26 @@ public class JpqlTest {
     EntityManager em;
 
     @Test
-    public void 생성(){
+    public void 생성() {
         Member member = new Member();
         member.setUsername("안주");
         em.persist(member);
+    }
+
+    @Test
+    public void 리스트반환() {
+        List<Member> memberList = em.createQuery("select m from Member m", Member.class).getResultList();
+        for (Member member : memberList) {
+            System.out.println(member.getUsername());
+        }
+    }
+
+    @Test
+    public void 타입쿼리() {
+        Member singleResult = em.createQuery("SELECT m FROM Member m where m.username = :username", Member.class)
+                .setParameter("username", "안주")
+                .getSingleResult();
+
+        System.out.println(singleResult.getUsername());
     }
 }
