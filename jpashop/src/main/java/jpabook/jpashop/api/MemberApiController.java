b18181927c3c26aeeef08ec3baaf7c43b8d8549d
dkswnkk <jjoy9810@gmail.com>
2022-03-19 22:55:10 +0900

    feat: 회원 수정 API 개발

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,22 +24,32 @@ public class MemberApiController {
     }
 
     @PostMapping("api/v2/members")
-    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.name);
         Long id = memberService.join(member);
         return new CreateMemberResponse(member.getId());
     }
 
+    @GetMapping("api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getId(), m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+
     @GetMapping("api/v1/members")
-    public FindMember findMember(@RequestParam @Valid Long memberId){
+    public FindMember findMember(@RequestParam @Valid Long memberId) {
         Member findMember = memberService.findOne(memberId);
         return new FindMember(findMember);
     }
 
     @PutMapping("api/v2/members/{id}")
     public UpdateMemberResponse updateMemberResponse(@PathVariable("id") Long id,
-                                                     @RequestBody @Valid UpdateMemberRequest updateMemberRequest){
+                                                     @RequestBody @Valid UpdateMemberRequest updateMemberRequest) {
         memberService.update(id, updateMemberRequest.getName());
         Member member = memberService.findOne(id);
         return new UpdateMemberResponse(member.getName());
@@ -45,16 +57,28 @@ public class MemberApiController {
     }
 
 
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private long count;
+        private T data;
+    }
 
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private long id;
+        private String name;
+    }
 
 
     @Data
-    static class UpdateMemberRequest{
+    static class UpdateMemberRequest {
         private String name;
     }
 
     @Data
-    static class UpdateMemberResponse{
+    static class UpdateMemberResponse {
         private String name;
         private String text = "성공적으로 업데이트 했습니다.";
 
@@ -64,13 +88,13 @@ public class MemberApiController {
     }
 
     @Data
-    static class CreateMemberRequest{
+    static class CreateMemberRequest {
         private String name;
         private String test;
     }
 
     @Data
-    static class FindMember{
+    static class FindMember {
         private Member member;
 
         public FindMember(Member member) {
