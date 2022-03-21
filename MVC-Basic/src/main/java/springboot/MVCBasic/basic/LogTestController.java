5a323b10631505a6d18571bb3de469c98e411fab
dkswnkk <jjoy9810@gmail.com>
2022-03-21 17:02:09 +0900

    feat: logging 찍어보기 https://big-marjoram-ffc.notion.site/Logging-3312a3e0639c4cd48814e6a0239b723f

---
+++ MVC-Basic/src/main/java/springboot/MVCBasic/basic/LogTestController.java
@@ -0,0 +1,24 @@
+package springboot.MVCBasic.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    @GetMapping("log-test")
+    public String logTest() {
+
+        String name = "Spring";
+        log.trace("trace log" + name);  // 이런 식으로 사용하면 안됨. 
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok";
+    }
+
+}
