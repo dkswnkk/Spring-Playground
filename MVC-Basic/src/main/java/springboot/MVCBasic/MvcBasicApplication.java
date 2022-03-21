8561eaf473d10367dd68c44da3140406484ea2cc
dkswnkk <jjoy9810@gmail.com>
2022-03-21 16:02:36 +0900

    chore: MVC 기초 프로젝트 생성

---
+++ MVC-Basic/src/main/java/springboot/MVCBasic/MvcBasicApplication.java
@@ -0,0 +1,13 @@
+package springboot.MVCBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBasicApplication.class, args);
+	}
+
+}
---
+++ MVC-Basic/src/test/java/springboot/MVCBasic/MvcBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.MVCBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
