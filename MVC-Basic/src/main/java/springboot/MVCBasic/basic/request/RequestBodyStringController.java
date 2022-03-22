02d398b373ed78f6c0700137c98724a5108a798c
dkswnkk <jjoy9810@gmail.com>
2022-03-22 23:57:27 +0900

    chore: HTTP 요청 메시지 - 단순 텍스트 https://big-marjoram-ffc.notion.site/e2bbccc0084b45439760f35f3a2b1e01

---
+++ MVC-Basic/src/main/java/springboot/MVCBasic/basic/request/RequestBodyStringController.java
@@ -0,0 +1,69 @@
+package springboot.MVCBasic.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request,
+                                  HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream,
+                StandardCharsets.UTF_8);
+        log.info("messageBody={}", messageBody);
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * InputStream(Reader): HTTP 요청 메시지 바디의 내용을 직접 조회 * OutputStream(Writer): HTTP 응답 메시지의 바디에 직접 결과 출력
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream,
+                StandardCharsets.UTF_8);
+        log.info("messageBody={}", messageBody);
+        responseWriter.write("ok");
+    }
+
+    /**
+     * HttpEntity: HTTP header, body 정보를 편라하게 조회
+     * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용 *
+     * 응답에서도 HttpEntity 사용 가능
+     * *
+     */
+// - 메시지 바디 정보 직접 반환(view 조회X)
+// - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+//        return new HttpEntity<>("ok");
+        return new ResponseEntity<String>("Ok", HttpStatus.CREATED);
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
+        log.info("messageBody={}", messageBody);
+        return "OK";
+    }
+
+}
