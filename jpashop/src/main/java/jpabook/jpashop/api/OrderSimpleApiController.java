1452e8f8e5b621040fc1e12d0b9414361d2d794b
dkswnkk <jjoy9810@gmail.com>
2022-03-30 23:14:46 +0900

    feat: 간단한 주문 조회 V1: 엔티티를 직접 노출 https://big-marjoram-ffc.notion.site/JPA-2-d9eecd4b61d945838628e958fc005903

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+/**
+ * xToOne
+ * Order
+ * order -> Member
+ * order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();    // Lazy 강제 초기화
+            order.getMember().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+
+}
