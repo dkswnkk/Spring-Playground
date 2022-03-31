bd8516ec95af5fa28e5c9103d3b49d0576b01966
dkswnkk <jjoy9810@gmail.com>
2022-03-31 17:18:38 +0900

    feat: 주문 조회 V1: 엔티티 직접 노출

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(orderItem -> orderItem.getItem().getName());
+        }
+        return all;
+    }
+
+
+}
