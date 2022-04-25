e30b8fa5280b6c15fb9349d18ca8ce21e3fd1055
dkswnkk <jjoy9810@gmail.com>
2022-04-25 23:24:37 +0900

    chore: API-Practice 프로젝트 생성 Dependencies 1. Spring Data JPA 2. Lombok 3. Spring Boot DevTools 4. H2 Database 5. Spring Web

---
+++ API-Practice/src/main/java/com/example/APIPractice/ApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracticeApplication.class, args);
+	}
+
+}
---
+++ API-Practice/src/test/java/com/example/APIPractice/ApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
