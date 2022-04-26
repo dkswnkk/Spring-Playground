74f08a41eeb2286925494ec3c6a0a4629eceba02
dkswnkk <jjoy9810@gmail.com>
2022-04-26 23:48:51 +0900

    feat: Spring의 AOP를 이용한 Exception Handling(@ControllerAdvice)

---
+++ API-Practice/src/main/java/com/example/APIPractice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.APIPractice.exception;
+
+import com.example.APIPractice.user.UserNotFountException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        log.info("Exception.class");
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFountException.class)
+    public final ResponseEntity<Object> handleUserNotFountException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        log.info("UserNotFountException.class");
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ API-Practice/src/main/java/com/example/APIPractice/exception/ExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.example.APIPractice.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
