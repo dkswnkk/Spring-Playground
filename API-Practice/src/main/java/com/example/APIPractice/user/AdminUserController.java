9313187c2683b6e04a415f9926759022482b904d
dkswnkk <jjoy9810@gmail.com>
2022-05-14 21:50:08 +0900

    feat: 전체 사용자 조회 @jsonfilter을 이용한 Response 데이터 Filtering

--- API-Practice/src/main/java/com/example/APIPractice/user/AdminUserController.java
+++ API-Practice/src/main/java/com/example/APIPractice/user/AdminUserController.java
@@ -25,12 +25,18 @@ public class AdminUserController {
     }
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers() {
+    public MappingJacksonValue retrieveAllUsers() {
         List<User> users = service.findAll();
-        for (User user : users) {
-            log.info("user1 정보 {}", user.toString());
-        }
-        return service.findAll();
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "password");
+
+
+        // @JsonFilter("UserInfo")
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(users);
+        mapping.setFilters(filters);
+        return mapping;
     }
 
     @GetMapping("/users/{id}")
