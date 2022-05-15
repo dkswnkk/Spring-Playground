8bbdc468133767509802bd8901168e8950853005
dkswnkk <jjoy9810@gmail.com>
2022-05-16 02:18:01 +0900

    feat: Configuration 클래스를 이용한 사용자 인증 처리

---
+++ API-Practice/src/main/java/com/example/APIPractice/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.APIPractice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("juhyeong")
+                .password("{noop}password")
+                .roles("USER");
+    }
+
+}
