d1439776226a0dc3fa121f9fa0d668a7a35252ea
dkswnkk <jjoy9810@gmail.com>
2022-05-17 21:24:34 +0900

    feat: JPA를 이용한 CRUD 학습

--- API-Practice/src/main/java/com/example/APIPractice/user/UserJpaController.java
+++ API-Practice/src/main/java/com/example/APIPractice/user/UserJpaController.java
@@ -1,21 +1,50 @@
 package com.example.APIPractice.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
+@Slf4j
 public class UserJpaController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent()) {
+            throw new UserNotFountException(String.format("ID: %s not found", id));
+        }
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User saveUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
