f160a1a4566c104d72da274e792dcaa60a796342
dkswnkk <jjoy9810@gmail.com>
2022-05-18 15:00:57 +0900

    chore: JAVA PLAYGROUND 프로젝트 생성

---
+++ JAVAPRACTICE/src/main/java/com/example/javapractice/JavapracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavapracticeApplication.class, args);
+    }
+
+}
---
+++ JAVAPRACTICE/src/test/java/com/example/javapractice/JavapracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavapracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
