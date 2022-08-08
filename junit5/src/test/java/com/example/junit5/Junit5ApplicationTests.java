da316eee9f216e55e8b09898bb3be1ae46392cb3
dkswnkk <jjoy9810@gmail.com>
2022-08-08 20:05:15 +0900

    test: Test Fail Message

--- junit5/src/test/java/com/example/junit5/Junit5ApplicationTests.java
+++ junit5/src/test/java/com/example/junit5/Junit5ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.junit5;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Junit5ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void 테스트1() {
+        String str = "가위";
+        Assertions.assertThat(str).as("값이 일치하지 않습니다. 현재 값: %s", str)
+                .isEqualTo("바위");
+    }
 
 }
