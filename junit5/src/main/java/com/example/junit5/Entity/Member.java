d76913f56e07416f2910c2db31c5795aead7a26a
dkswnkk <jjoy9810@gmail.com>
2022-08-08 20:01:12 +0900

    feat: initial commit

---
+++ junit5/src/main/java/com/example/junit5/Entity/Member.java
@@ -0,0 +1,14 @@
+package com.example.junit5.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+}
