5d1fcfd881bdacbfe190ad222f267b180d1e9559
JuHyeong Ahn <jjoy9810@gmail.com>
2022-08-30 18:56:44 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import feedparser, time
+
+URL="https://dkswnkk.tistory.com/rss"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST=1000
+
+markdown_text = """
+## ✅ Latest Blog Post
+
+""" # list of blog posts will be appended here
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        tags = feed['tags'][0]['term']
+        if tags == 'Spring' or tags == 'Java' or tags == 'Infra' or tags == 'Git & GitHub':
+            feed_date = feed['published_parsed']
+            markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        f = open("README.md", mode="w", encoding="utf-8")
+        f.write(markdown_text)
+        f.close()
