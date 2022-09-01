914cb5dae54cce11510e972b86dc94b59b32695a
dkswnkk <jjoy9810@gmail.com>
2022-09-01 15:15:14 +0900

    feat: 사용자 정의 리포지토리 생성

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -5,6 +5,6 @@ import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     List<Member> findByUsername(String username);
 }
---
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -0,0 +1,10 @@
+package study.querydsl.repository;
+
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+}
