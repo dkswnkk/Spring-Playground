155520bcf7253d3fe0bf9927c37b9f3eaafe3a11
dkswnkk <jjoy9810@gmail.com>
2022-09-01 18:33:28 +0900

    feat: 인터페이스 지원 - QuerydslPredicateExecutor 한계점 1. 조인이 되지 않는다. (묵시적 조인은 가능하지만 left join이 불가능하다.) 2. 클라이언트가 Querydsl에 의존해야 한다. 서비스 클래스가 Querydsl이라는 구현 기술에 의존해야 한다. 복잡한 실무환경에서 사용하기에는 한계가 명확하다.

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     List<Member> findByUsername(String username);
 }
--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryCustomTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryCustomTest.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -58,6 +59,32 @@ class MemberRepositoryCustomTest {
         assertThat(result.getContent())
                 .extracting("username")
                 .containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(
+                QMember.member.age.between(0, 40)
+                        .and(QMember.member.username.eq("member1")).or(QMember.member.username.eq("member2")));
+
+        for (Member member : result) {
+            System.out.println(member.getUsername());
+        }
     }
 
 }
