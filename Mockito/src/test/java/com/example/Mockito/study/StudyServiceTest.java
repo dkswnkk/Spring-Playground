620d0597b2d031d2a85cae8851ca499fdacf4c97
dkswnkk <jjoy9810@gmail.com>
2022-10-02 12:48:12 +0900

    refactor: Mockito를 사용하여 Mock객체 생성

--- Mockito/src/test/java/com/example/Mockito/study/StudyServiceTest.java
+++ Mockito/src/test/java/com/example/Mockito/study/StudyServiceTest.java
@@ -4,192 +4,20 @@ import com.example.Mockito.domain.Member;
 import com.example.Mockito.domain.Study;
 import com.example.Mockito.member.MemberService;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
+import org.mockito.Mockito;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class StudyServiceTest {
 
     @Test
     void createStudyService() {
-        MemberService memberService = new MemberService() {
-            @Override
-            public Optional<Member> findById(Long memberId) {
-                return Optional.empty();
-            }
+        MemberService memberService = mock(MemberService.class);
+        StudyRepository studyRepository = mock(StudyRepository.class);
 
-            @Override
-            public void validate(Long memberId) {
-            }
-
-            @Override
-            public void notify(Study newStudy) {
-            }
-
-            @Override
-            public void notify(Member member) {
-            }
-        };
-
-        StudyRepository studyRepository = new StudyRepository() {
-            @Override
-            public List<Study> findAll() {
-                return null;
-            }
-
-            @Override
-            public List<Study> findAll(Sort sort) {
-                return null;
-            }
-
-            @Override
-            public List<Study> findAllById(Iterable<Long> longs) {
-                return null;
-            }
-
-            @Override
-            public <S extends Study> List<S> saveAll(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public void flush() {
-
-            }
-
-            @Override
-            public <S extends Study> S saveAndFlush(S entity) {
-                return null;
-            }
-
-            @Override
-            public <S extends Study> List<S> saveAllAndFlush(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public void deleteAllInBatch(Iterable<Study> entities) {
-
-            }
-
-            @Override
-            public void deleteAllByIdInBatch(Iterable<Long> longs) {
-
-            }
-
-            @Override
-            public void deleteAllInBatch() {
-
-            }
-
-            @Override
-            public Study getOne(Long aLong) {
-                return null;
-            }
-
-            @Override
-            public Study getById(Long aLong) {
-                return null;
-            }
-
-            @Override
-            public Study getReferenceById(Long aLong) {
-                return null;
-            }
-
-            @Override
-            public <S extends Study> List<S> findAll(Example<S> example) {
-                return null;
-            }
-
-            @Override
-            public <S extends Study> List<S> findAll(Example<S> example, Sort sort) {
-                return null;
-            }
-
-            @Override
-            public Page<Study> findAll(Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends Study> S save(S entity) {
-                return null;
-            }
-
-            @Override
-            public Optional<Study> findById(Long aLong) {
-                return Optional.empty();
-            }
-
-            @Override
-            public boolean existsById(Long aLong) {
-                return false;
-            }
-
-            @Override
-            public long count() {
-                return 0;
-            }
-
-            @Override
-            public void deleteById(Long aLong) {
-
-            }
-
-            @Override
-            public void delete(Study entity) {
-
-            }
-
-            @Override
-            public void deleteAllById(Iterable<? extends Long> longs) {
-
-            }
-
-            @Override
-            public void deleteAll(Iterable<? extends Study> entities) {
-
-            }
-
-            @Override
-            public void deleteAll() {
-
-            }
-
-            @Override
-            public <S extends Study> Optional<S> findOne(Example<S> example) {
-                return Optional.empty();
-            }
-
-            @Override
-            public <S extends Study> Page<S> findAll(Example<S> example, Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends Study> long count(Example<S> example) {
-                return 0;
-            }
-
-            @Override
-            public <S extends Study> boolean exists(Example<S> example) {
-                return false;
-            }
-
-            @Override
-            public <S extends Study, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-                return null;
-            }
-        };
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
     }
