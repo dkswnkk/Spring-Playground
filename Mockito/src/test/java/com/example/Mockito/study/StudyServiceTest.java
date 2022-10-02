1ef1cb54609a6808e843221c58d57efb3005d718
dkswnkk <jjoy9810@gmail.com>
2022-10-02 12:57:34 +0900

    refactor: ExtendWith에 MockitoExtension.class 설정  1. ExtendWith에 MockitoExtension.class 설정 2. @Mock 애노테이션 설정 => MemberService memberService = Mock(MemberService.class)와 같이 생성하지 않아도 됨.

--- Mockito/src/test/java/com/example/Mockito/study/StudyServiceTest.java
+++ Mockito/src/test/java/com/example/Mockito/study/StudyServiceTest.java
@@ -4,19 +4,30 @@ import com.example.Mockito.domain.Member;
 import com.example.Mockito.domain.Study;
 import com.example.Mockito.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
+    /*
+        1. ExtendWith에 MockitoExtension.class 설정
+        2. @Mock 애노테이션 설정
+        => MemberService memberService = Mock(MemberService.class)와 같이 생성하지 않아도 됨.
+     */
+    @Mock
+    MemberService memberService;
+    @Mock
+    StudyRepository studyRepository;
 
     @Test
     void createStudyService() {
-        MemberService memberService = mock(MemberService.class);
-        StudyRepository studyRepository = mock(StudyRepository.class);
 
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
