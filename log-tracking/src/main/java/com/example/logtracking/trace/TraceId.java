0d8995f19d9a6ac2b88b7076803c6fc4209e8f59
dkswnkk <jjoy9810@gmail.com>
2022-11-02 00:03:07 +0900

    feat: 로그 추적기 V1 - 프로토타입 개발

---
+++ log-tracking/src/main/java/com/example/logtracking/trace/TraceId.java
@@ -0,0 +1,43 @@
+package com.example.logtracking.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private final String id;
+    private final int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    private TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    private TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
