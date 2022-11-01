84c38ddbd3a0e2bcf9fa10395854dadebee7cd4c
dkswnkk <jjoy9810@gmail.com>
2022-11-01 21:28:11 +0900

    init: Spring log tracking - 프로젝트 생성

---
+++ log-tracking/src/main/java/com/example/logtracking/LogTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.logtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTrackingApplication.class, args);
+	}
+
+}
---
+++ log-tracking/src/test/java/com/example/logtracking/LogTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
