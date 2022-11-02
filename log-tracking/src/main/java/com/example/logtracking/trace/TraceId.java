1913de99e499398358d77d8100a6a13c1b6aa74b
dkswnkk <jjoy9810@gmail.com>
2022-11-02 18:28:23 +0900

    refactor: createNextId, createPreviousId 접근 제어자 변경

--- log-tracking/src/main/java/com/example/logtracking/trace/TraceId.java
+++ log-tracking/src/main/java/com/example/logtracking/trace/TraceId.java
@@ -21,11 +21,11 @@ public class TraceId {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    private TraceId createNextId() {
+    public TraceId createNextId() {
         return new TraceId(id, level + 1);
     }
 
-    private TraceId createPreviousId() {
+    public TraceId createPreviousId() {
         return new TraceId(id, level - 1);
     }
 
