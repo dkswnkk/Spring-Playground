aff77ef63f6d6c5dcc488d0406c5c55004307f23
dkswnkk <jjoy9810@gmail.com>
2022-11-04 17:14:14 +0900

    test: 동시성 문제 코드 작성 1. 지역변수는 쓰레드마다 각각 다른 메모리 영역이 할당되기 때문에 동시성 문제가 발생하지 않는다. 2. 동시성 문제가 발생하는 곳은 같은 인스턴스의 필드(주 싱글톤에서 자주 발생), 또는 static 같은 공용 필드에 접근할 때 발생한다. 3. 동시성 문제는 값을 읽기만 하면 발생하지 않는다. 어디선가 값을 변경하기 때문에 발생한다.

--- log-tracking/src/main/java/com/example/logtracking/trace/TraceStatus.java
+++ log-tracking/src/main/java/com/example/logtracking/trace/TraceStatus.java
@@ -1,9 +1,9 @@
 package com.example.logtracking.trace;
 
 public class TraceStatus {
-    private TraceId traceId;
-    private Long startTimeMs;
-    private String message;
+    private final TraceId traceId;
+    private final Long startTimeMs;
+    private final String message;
 
     public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
         this.traceId = traceId;
---
+++ log-tracking/src/test/java/com/example/logtracking/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.logtracking.trace.threadlocal;
+
+import com.example.logtracking.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private final FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);    // 동시성 발생 x
+        sleep(100); // 동시성 발생 o
+        threadB.start();
+        sleep(3000);    // 메인 쓰레드 종료 대기
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
