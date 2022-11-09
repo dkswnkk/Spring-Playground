1d475cc5734c92dcde47eeeada66dab0734cc4af
dkswnkk <jjoy9810@gmail.com>
2022-11-10 02:02:53 +0900

    test: 템플릿 메서드 패턴 - 예제1 템플릿 메서드 패턴을 사용해서 변하는 부분과 변하지 않는 부분을 분리해보자.

--- log-tracking/src/main/java/com/example/logtracking/LogTraceConfig.java
+++ log-tracking/src/main/java/com/example/logtracking/LogTraceConfig.java
@@ -2,6 +2,7 @@ package com.example.logtracking;
 
 import com.example.logtracking.trace.logtrace.FieldLogTrace;
 import com.example.logtracking.trace.logtrace.LogTrace;
+import com.example.logtracking.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
---
+++ log-tracking/src/test/java/com/example/logtracking/trace/template/TemplateMethodTest.java
@@ -0,0 +1,39 @@
+package com.example.logtracking.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.net.SocketTimeoutException;
+/*
+    템플릿 메서드 패턴을 사용해서 변하는 부분과 변하지 않는 부분을 분리해보자.
+ */
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+}
