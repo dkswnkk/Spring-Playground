cdd3a2afa14aac9a5d512e22592c5be3e4506e74
dkswnkk <jjoy9810@gmail.com>
2022-11-10 14:36:27 +0900

    test: 익명 클래스를 활용하여 추상 메서드 상속

--- log-tracking/src/test/java/com/example/logtracking/trace/template/TemplateMethodTest.java
+++ log-tracking/src/test/java/com/example/logtracking/trace/template/TemplateMethodTest.java
@@ -52,4 +52,25 @@ public class TemplateMethodTest {
         template2.execute();
     }
 
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        log.info("클래스 이름 = {}", template2.getClass());
+        template2.execute();
+    }
+
 }
