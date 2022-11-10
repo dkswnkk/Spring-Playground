2e94dce01f22b460611049c8c0c03c0b196d2c22
dkswnkk <jjoy9810@gmail.com>
2022-11-10 17:32:41 +0900

    test: 전략 패턴 - 예제1

---
+++ log-tracking/src/test/java/com/example/logtracking/trace/strategy/ContextV1Test.java
@@ -0,0 +1,51 @@
+package com.example.logtracking.trace.strategy;
+
+import com.example.logtracking.trace.strategy.code.strategy.ContextV1;
+import com.example.logtracking.trace.strategy.code.strategy.StrategyLogic1;
+import com.example.logtracking.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    @Test
+    @DisplayName("전략 패턴 사용1")
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+}
---
+++ log-tracking/src/test/java/com/example/logtracking/trace/strategy/code/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package com.example.logtracking.trace.strategy.code.strategy;
+
+public interface Strategy {
+    void call();
+}
