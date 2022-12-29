60e84faecf7420b3f4da852b84b1efc32cb04d4b
dkswnkk <jjoy9810@gmail.com>
2022-12-29 14:10:20 +0900

    feat: not use decorator pattern

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,20 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    @DisplayName("no decorator test")
+    void noDecorator() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+}
