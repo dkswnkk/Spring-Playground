ebb81aa9846d49e8ab25725753442a5823cb1917
dkswnkk <jjoy9810@gmail.com>
2022-12-29 14:22:45 +0900

    feat: use decorator pattern - MessageDecorator

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.decorator;
 
 import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,18 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    /**
+     * 실행 결과를 보면 MessageDecorator가 RealComponent를 호출하고 반환한 응답 메시지를 꾸며서 반환한 것을 확인할 수 있다.
+     */
+    @Test
+    @DisplayName("MessageDecorator test")
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageComponent = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
         client.execute();
     }
 }
