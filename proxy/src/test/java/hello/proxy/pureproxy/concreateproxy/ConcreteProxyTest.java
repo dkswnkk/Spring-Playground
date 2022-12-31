b50e5767189b688105a59af07a04e100d84af621
dkswnkk <jjoy9810@gmail.com>
2022-12-31 17:40:44 +0900

    feat: 구체 클래스 기반 프록시 - 프록시 적용x

---
+++ proxy/src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteProxyTest.java
@@ -0,0 +1,17 @@
+package hello.proxy.pureproxy.concreateproxy;
+
+import hello.proxy.pureproxy.concreateproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreateproxy.code.ConcreteLogic;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+
+    @Test
+    @DisplayName("no proxy test")
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+}
---
+++ proxy/src/test/java/hello/proxy/pureproxy/concreateproxy/code/ConcreteLogic.java
@@ -0,0 +1,13 @@
+package hello.proxy.pureproxy.concreateproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreteLogic {
+
+    public String operation() {
+        log.info("ConcreteLogic 실행");
+        return "data";
+    }
+
+}
