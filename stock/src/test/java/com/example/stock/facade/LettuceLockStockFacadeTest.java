b7ba927980032a38e946c886260d963296cceea2
dkswnkk <jjoy9810@gmail.com>
2023-01-04 22:58:07 +0900

    style: Lettuce 및 Redisson 정리

--- stock/src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
+++ stock/src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
@@ -15,6 +15,14 @@ import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * 구현이 간단하다.
+ * Spring Data Redis를 이용하면 Lettuce가 기본이기 때문에 별도의 라이브러리를 사용하지 않아도 된다.
+ * Spin Lock 방식이기 때문에 동시에 많은 스레드가 Lock 획득 대기 상태라면 Redis에 부하가 갈 수 있다.
+ * 실무에서는 재시도가 필요한 Lock의 경우에는 Redission을 활용하고, 그렇지 않을 경우에는 Lettuce을 활용한다.
+ * 재시도가 필요한 경우?: 선착순 100명 까지 물품을 구매할 수 있을 경우
+ * 재시도가 필요하지 않은 경우?: 선착순 한명만 가능, Lock 획득 재시도 할 필요가 없음
+ */
 @SpringBootTest
 class LettuceLockStockFacadeTest {
 
--- stock/src/test/java/com/example/stock/facade/RedissonLockStockFacadeTest.java
+++ stock/src/test/java/com/example/stock/facade/RedissonLockStockFacadeTest.java
@@ -15,6 +15,16 @@ import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+/**
+ * Lock 획득 재시도를 기본으로 제공한다.
+ * pub-sub 방식으로 구현이 되어있기 때문에 Lettuce와 비교했을 때 Redis에 부하가 덜 간다.
+ * 별도의 라이브러리를 사용해야 한다.
+ * Lock을 라이브러리 차원에서 제공해주기 때문에 사용법을 공부해야 한다.
+ * 실무에서는 재시도가 필요한 Lock의 경우에는 Redission을 활용하고, 그렇지 않을 경우에는 Lettuce을 활용한다.
+ * 재시도가 필요한 경우?: 선착순 100명 까지 물품을 구매할 수 있을 경우
+ * 재시도가 필요하지 않은 경우?: 선착순 한명만 가능, Lock 획득 재시도 할 필요가 없음
+ */
 @SpringBootTest
 class RedissonLockStockFacadeTest {
 
