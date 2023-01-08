744725b4085e4e6310d32f377542815c361fee20
dkswnkk <jjoy9810@gmail.com>
2023-01-08 21:51:56 +0900

    init: batch-insert-test

---
+++ bath-insert-test/src/main/java/com/example/bathinserttest/BathInsertTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bathinserttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BathInsertTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BathInsertTestApplication.class, args);
+	}
+
+}
---
+++ bath-insert-test/src/test/java/com/example/bathinserttest/BathInsertTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bathinserttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BathInsertTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
