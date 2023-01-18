71e3579e19177c20e3b8e13a52f3ec6e6ee4a831
dkswnkk <jjoy9810@gmail.com>
2023-01-18 22:05:35 +0900

    feat: Spring Batch Bean 주입

--- spring-batch/src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ spring-batch/src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,9 +1,11 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
