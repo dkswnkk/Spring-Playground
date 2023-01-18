76cf560b426a367d2acffcfef64c625e7ae98ddf
dkswnkk <jjoy9810@gmail.com>
2023-01-18 22:53:48 +0900

    feat: batch-jdbc-initialize 'always' 설정

--- spring-batch/src/main/java/com/example/springbatch/config/HelloJobConfig.java
+++ spring-batch/src/main/java/com/example/springbatch/config/HelloJobConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class HelloJobConfig {
 
     private final JobBuilderFactory jobBuilderFactory;
