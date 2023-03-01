a975a1aeecd5f2ac30e1fbdd62852c3a69ac1b90
dkswnkk <jjoy9810@gmail.com>
2023-03-01 22:07:47 +0900

    feat: Form Login 세부 기능 구현

--- spring-security/src/main/java/com/example/demo/SecurityConfig.java
+++ spring-security/src/main/java/com/example/demo/SecurityConfig.java
@@ -15,7 +15,22 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .anyRequest()
                 .authenticated();
-        http.formLogin();
+        http.formLogin()
+                .loginPage("/loginPage")    // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/")     // 로그인 성공 후 이동 페이지
+                .failureUrl("/login")       // 로그인 실패 후 이동 페이지
+                .usernameParameter("userId")    // 아이디 파라미터명 설정
+                .passwordParameter("pwd")   // 패스워드 파라미터명 설정
+                .loginProcessingUrl("/login_proc")  // 로그인 Form Action Url
+                .successHandler((request, response, authentication) -> {    // 로그인 성공 후 핸들러
+                    System.out.println("authentication" + authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {     // 로그인 실패 후 핸들러
+                    System.out.println("exception" + exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+                .permitAll();
         return http.build();
     }
 }
--- spring-security/src/main/java/com/example/demo/SecurityController.java
+++ spring-security/src/main/java/com/example/demo/SecurityController.java
@@ -11,4 +11,9 @@ public class SecurityController {
         return "home";
     }
 
+    @GetMapping("loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
 }
