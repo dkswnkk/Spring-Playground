9fb6590a7aebd7ed3807e76e9bff59fe4581dea4
JuHyeong Ahn <jjoy9810@gmail.com>
2023-03-23 11:04:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         break
     else:
         tags = feed['tags'][0]['term']
-        if tags == 'Spring' or tags == 'Java' or tags == 'Infra' or tags == 'Git & GitHub':
+        if tags == 'Spring' or tags == 'Java' or tags == 'Infra' or tags == 'Git & GitHub' or tags == 'SQL':
             feed_date = feed['published_parsed']
             markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
         f = open("README.md", mode="w", encoding="utf-8")
