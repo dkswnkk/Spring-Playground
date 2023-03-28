d4761dfd314d3a920cefa14cf1790ef2a9d974e0
dkswnkk <jjoy9810@gmail.com>
2023-03-29 00:14:46 +0900

    feat: 로그아웃 처리

--- spring-security/src/main/java/com/example/demo/SecurityConfig.java
+++ spring-security/src/main/java/com/example/demo/SecurityConfig.java
@@ -4,7 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -16,12 +25,12 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated();
         http.formLogin()
-                .loginPage("/loginPage")    // 사용자 정의 로그인 페이지
+//                .loginPage("/loginPage")    // 사용자 정의 로그인 페이지
                 .defaultSuccessUrl("/")     // 로그인 성공 후 이동 페이지
                 .failureUrl("/login")       // 로그인 실패 후 이동 페이지
                 .usernameParameter("userId")    // 아이디 파라미터명 설정
                 .passwordParameter("pwd")   // 패스워드 파라미터명 설정
-                .loginProcessingUrl("/login_proc")  // 로그인 Form Action Url
+//                .loginProcessingUrl("/login_proc")  // 로그인 Form Action Url
                 .successHandler((request, response, authentication) -> {    // 로그인 성공 후 핸들러
                     System.out.println("authentication" + authentication.getName());
                     response.sendRedirect("/");
@@ -31,6 +40,27 @@ public class SecurityConfig {
                     response.sendRedirect("/login");
                 })
                 .permitAll();
+        http.logout()   // 로그아웃 처리
+                .logoutUrl("/logout")      //  로그아웃처리 URL
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동페이지
+                .addLogoutHandler(  // 로그아웃 핸들러
+                        new LogoutHandler() {
+                            @Override
+                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                                HttpSession session = request.getSession();
+                                session.invalidate();
+                            }
+                        }
+                )
+                .logoutSuccessHandler(  // 로그아웃 성공 후 핸들러
+                        new LogoutSuccessHandler() {
+                            @Override
+                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                response.sendRedirect("/login");
+                            }
+                        }
+                )
+                .deleteCookies("remember-me");
         return http.build();
     }
 }
