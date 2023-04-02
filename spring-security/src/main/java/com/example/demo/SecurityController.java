ddfd336711d2097e3d94f9d79157bd9a2c4886a1
dkswnkk <jjoy9810@gmail.com>
2023-04-03 00:50:29 +0900

    feat: Controller 엔드포인트 추가

--- spring-security/src/main/java/com/example/demo/SecurityController.java
+++ spring-security/src/main/java/com/example/demo/SecurityController.java
@@ -16,4 +16,29 @@ public class SecurityController {
         return "loginPage";
     }
 
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+
+    @GetMapping("admin/**")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "Access is denied";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        return "login";
+    }
+
 }
