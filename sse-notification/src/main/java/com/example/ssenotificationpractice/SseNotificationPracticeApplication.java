53250722544a39a8655804f49b3779f61b896869
dkswnkk <jjoy9810@gmail.com>
2023-06-18 14:13:10 +0900

    init: 프로젝트 생성

---
+++ sse-notification/src/main/java/com/example/ssenotificationpractice/SseNotificationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssenotificationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseNotificationPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseNotificationPracticeApplication.class, args);
+	}
+
+}
---
+++ sse-notification/src/test/java/com/example/ssenotificationpractice/SseNotificationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssenotificationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseNotificationPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
