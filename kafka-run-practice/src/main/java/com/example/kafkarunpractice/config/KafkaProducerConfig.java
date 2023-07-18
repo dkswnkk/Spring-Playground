03b9d3cd2a3288c165b3329665517586889eb41d
dkswnkk <jjoy9810@gmail.com>
2023-07-19 00:00:35 +0900

    refactor: StringDeserializer.class -> StringSerializer.class

--- kafka-run-practice/src/main/java/com/example/kafkarunpractice/config/KafkaProducerConfig.java
+++ kafka-run-practice/src/main/java/com/example/kafkarunpractice/config/KafkaProducerConfig.java
@@ -20,7 +20,7 @@ public class KafkaProducerConfig {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
