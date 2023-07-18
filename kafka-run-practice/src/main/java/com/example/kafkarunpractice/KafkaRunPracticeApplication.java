6156228894099aa5e0b1cd48eb94ebc59eadd88c
dkswnkk <jjoy9810@gmail.com>
2023-07-18 22:32:25 +0900

    init: kafka-run-practice

---
+++ kafka-run-practice/src/main/java/com/example/kafkarunpractice/KafkaRunPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkarunpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRunPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRunPracticeApplication.class, args);
+	}
+
+}
